2f0d05fe9cd968718714912fff4ce8d69aa9f3a6
Andrew Lee <candrewlee14@gmail.com>
2024-02-04 12:09:59 -0700

    add functions for getting window size

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,69 @@
 const std = @import("std");
-const c = @cImport(
-    @cInclude("termios.h"),
-);
+const c = @cImport({
+    @cInclude("termios.h");
+    @cInclude("sys/ioctl.h");
+});
+
+const Rect = struct {
+    x: usize,
+    y: usize,
+    width: usize,
+    height: usize,
+};
+
+const Size = struct {
+    width: usize,
+    height: usize,
+};
+
+const Position = struct {
+    x: usize,
+    y: usize,
+};
 
 const Terminal = struct {
     orig_termios: std.os.termios = undefined,
+
+    pub fn getCursorPos(self: *Terminal) !Position {
+        _ = self;
+        const stdout = std.io.getStdOut();
+        const stdin = std.io.getStdIn();
+        const sw = stdout.writer();
+        const sr = stdin.reader();
+        try sw.writeAll("\x1b[6n");
+        var buf: [32]u8 = undefined;
+        var i: usize = 0;
+        while (i < buf.len - 1) {
+            const byte = sr.readByte() catch |err| {
+                if (err == error.EndOfStream) {
+                    continue;
+                } else {
+                    return err;
+                }
+            };
+            buf[i] = byte;
+            if (byte == 'R') break;
+            i += 1;
+        }
+        buf[i] = 0;
+        if (buf[0] != '\x1b' or buf[1] != '[') return error.InvalidUnicodeCodepoint;
+        const semi_idx = std.mem.indexOfScalar(u8, &buf, ';') orelse return error.InvalidUnicodeCodepoint;
+        const x = try std.fmt.parseUnsigned(usize, buf[2..], 10);
+        const y = try std.fmt.parseUnsigned(usize, buf[semi_idx + 1 ..], 10);
+        return Position{ .x = x, .y = y };
+    }
+
+    pub fn getSize(self: *Terminal) !Size {
+        var ws: std.os.linux.winsize = undefined;
+        const stdout = std.io.getStdOut();
+        const sw = stdout.writer();
+        if (std.c.ioctl(stdout.handle, c.TIOCGWINSZ, &ws) == -1 or ws.ws_col == 0) {
+            try sw.writeAll("\x1b[999C\x1b[999B");
+            const cursor_pos = try self.getCursorPos();
+            return Size{ .width = cursor_pos.x, .height = cursor_pos.y };
+        }
+        return Size{ .width = ws.ws_col, .height = ws.ws_row };
+    }
 
     pub fn initScreen(self: *Terminal) !void {
         _ = self;
@@ -13,6 +72,7 @@ const Terminal = struct {
         const alternate_screen_code = "\x1b[?1049h";
         try sw.writeAll(alternate_screen_code);
     }
+
     pub fn deinitScreen(self: *Terminal) void {
         _ = self;
         const stdout = std.io.getStdOut();
@@ -20,6 +80,29 @@ const Terminal = struct {
         const exit_alternate_screen_code = "\x1b[?1049l";
         sw.writeAll(exit_alternate_screen_code) catch unreachable;
     }
+
+    pub fn readKey(self: *Terminal) !u8 {
+        _ = self;
+        const stdin = std.io.getStdIn();
+        const reader = stdin.reader();
+        while (true) {
+            return reader.readByte() catch |err| switch (err) {
+                error.EndOfStream => continue,
+                else => return err,
+            };
+        }
+    }
+
+    pub fn clear(self: *Terminal) !void {
+        _ = self;
+        const stdout = std.io.getStdOut();
+        const sw = stdout.writer();
+        const clear_screen_code = "\x1b[2J";
+        try sw.writeAll(clear_screen_code);
+        const cursor_home_code = "\x1b[H";
+        try sw.writeAll(cursor_home_code);
+    }
+
     pub fn enableRawMode(self: *Terminal) !void {
         const stdout = std.io.getStdOut();
         // save the original terminal settings and set our own
@@ -67,23 +150,12 @@ const Editor = struct {
     pub fn init(self: *Editor) !void {
         try self.terminal.initScreen();
         try self.terminal.enableRawMode();
+        try self.terminal.clear();
     }
 
     pub fn deinit(self: *Editor) void {
         self.terminal.deinitScreen();
         self.terminal.disableRawMode();
-    }
-
-    pub fn readKey(self: *Editor) !u8 {
-        _ = self;
-        const stdin = std.io.getStdIn();
-        const reader = stdin.reader();
-        while (true) {
-            return reader.readByte() catch |err| switch (err) {
-                error.EndOfStream => continue,
-                else => return err,
-            };
-        }
     }
 };
 
@@ -94,8 +166,10 @@ pub fn main() !void {
     defer ed.deinit();
 
     std.debug.print("All your {s} are belong to us.\n\r", .{"codebase"});
+    const size = try ed.terminal.getSize();
+    std.debug.print("Screen size: {d}x{d}\n\r", .{ size.width, size.height });
     while (true) {
-        const byte = try ed.readKey();
+        const byte = try ed.terminal.readKey();
         if (std.ascii.isControl(byte)) {
             std.debug.print("{d}\n\r", .{byte});
         } else {
